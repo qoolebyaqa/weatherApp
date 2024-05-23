75256a94e941b8642ae03c4b56b304791e6c2814
Arthur <goolebyaqa@gmail.com>
2024-05-23 17:34:03 +0500

    fix: try to deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 
 export default defineNuxtConfig({
+  nitro: {
+    preset: 'static'
+  },
+  app: {
+    baseURL: '/weatherApp/'
+  },
   runtimeConfig: {
     public: {
       apiKey: process.env.NUXT_PUBLIC_API_KEY
