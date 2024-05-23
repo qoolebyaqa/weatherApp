44e7614171a5e8d6c236dd5311bef1e3ca308f35
Arthur <goolebyaqa@gmail.com>
2024-05-23 17:51:32 +0500

    fix: some changes in configuration files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,5 @@
 
 export default defineNuxtConfig({
-  nitro: {
-    preset: 'static'
-  },
-  app: {
-    baseURL: '/weatherApp/'
-  },
   runtimeConfig: {
     public: {
       apiKey: process.env.NUXT_PUBLIC_API_KEY
